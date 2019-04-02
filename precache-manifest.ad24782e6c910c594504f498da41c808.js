71d13288eb1630716158c45c37831a782a3b0559
Jon Jackson <ocskier@gmail.com>
2019-04-02 17:12:56 -0400

    Updates

---
+++ precache-manifest.ad24782e6c910c594504f498da41c808.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0982becd4e30d7bdd79a",
+    "url": "/BLS-Website/static/css/main.08e672eb.chunk.css"
+  },
+  {
+    "revision": "0982becd4e30d7bdd79a",
+    "url": "/BLS-Website/static/js/main.0982becd.chunk.js"
+  },
+  {
+    "revision": "b2b98cec26dc45fccf86",
+    "url": "/BLS-Website/static/js/runtime~main.b2b98cec.js"
+  },
+  {
+    "revision": "dd8863bbbd7a5cd5546b",
+    "url": "/BLS-Website/static/js/2.dd8863bb.chunk.js"
+  },
+  {
+    "revision": "ebe803f51cd086d63fccf365b1b87dee",
+    "url": "/BLS-Website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/BLS-Website/precache-manifest.ae0433dc3e446660c0476bf96d98c4e5.js"
+  "/BLS-Website/precache-manifest.ad24782e6c910c594504f498da41c808.js"
 );
 
 workbox.clientsClaim();
