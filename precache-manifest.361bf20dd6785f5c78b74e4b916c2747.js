ec9653edc964c8ffa2bf93e1000dc1c970510251
Jon Jackson <ocskier@gmail.com>
2019-04-05 23:35:28 -0400

    Updates

---
+++ precache-manifest.361bf20dd6785f5c78b74e4b916c2747.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e7e20ee6146d34cda0f",
+    "url": "/BLS-Website/static/css/main.98097708.chunk.css"
+  },
+  {
+    "revision": "3e7e20ee6146d34cda0f",
+    "url": "/BLS-Website/static/js/main.3e7e20ee.chunk.js"
+  },
+  {
+    "revision": "b2b98cec26dc45fccf86",
+    "url": "/BLS-Website/static/js/runtime~main.b2b98cec.js"
+  },
+  {
+    "revision": "42c2679474f2fd4b8820",
+    "url": "/BLS-Website/static/js/2.42c26794.chunk.js"
+  },
+  {
+    "revision": "5617850c97df41dcb4f1db51676685f2",
+    "url": "/BLS-Website/index.html"
+  }
+];
